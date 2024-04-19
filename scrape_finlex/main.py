d3850803681548464608ac1915fd3062bc99eb53
bwallker <benbwall@gmail.com>
2024-04-19 17:39:04 +0300

    Generalize

--- scrape_finlex/main.py
+++ scrape_finlex/main.py
@@ -1,20 +1,40 @@
 """"The main module in scrape-finlex."""
 
-from typing import Any, Iterable
+from typing import Iterable
 from bs4 import BeautifulSoup, PageElement, Tag
 from requests import get, Response
 from dataclasses import dataclass
-from itertools import batched, chain
+from itertools import batched
 from multiprocessing import Pool
+from dotenv import load_dotenv
+from os import getenv
 
 
-def get_page(offset: int) -> Response:
+def get_page(offset: int, link: str) -> Response:
     """Get a page from Finlex."""
-    if offset < 0 or offset > 780:
-        raise ValueError("Offset must be between 0 and 780.")
-    return get(
-        f"https://www.finlex.fi/fi/sopimukset/sopsviite/haku/?search%5Btype%5D=luokitus&search%5Btype1%5D=Monenv%C3%A4liset%20sopimukset&_offset={offset}"
-    )
+    if offset < 0 or offset > 1090:
+        raise ValueError("Offset must be between 0 and 1090.")
+    return get(f"{link}&_offset={offset}")
+
+
+@dataclass
+class Config:
+    """Configuration class representing configuration read from dotenv file."""
+
+    link: str
+    output_file_name: str
+
+
+def parse_config() -> Config:
+    """Parse configuration from dotenv file."""
+    load_dotenv()
+    link = getenv("LINK")
+    if link is None:
+        raise ValueError("LINK environment variable must be set.")
+    output_file_name = getenv("OUTPUT_FILE_NAME")
+    if output_file_name is None:
+        raise ValueError("OUTPUT_FILE_NAME environment variable must be set.")
+    return Config(link, output_file_name)
 
 
 @dataclass
@@ -85,27 +105,66 @@ def parse_page(soup: BeautifulSoup) -> Iterable[DocumentEntry]:
     return entries
 
 
-def pass_(offset: int) -> list[DocumentEntry]:
+def pass_(offset: int, link: str) -> list[DocumentEntry]:
     """Run a pass."""
     print("Running pass number", offset)
-    res = get_page(offset)
+    res = get_page(offset, link)
     soup = BeautifulSoup(res.text, features="html.parser")
     entries = list(parse_page(soup))
     print("Finished running pass number", offset)
     return entries
 
 
+def get_num_pages(link: str) -> int:
+    """Get the number of pages."""
+    res = get_page(0, link)
+    soup = BeautifulSoup(res.text, features="html.parser")
+    super_div = soup.find(class_="result-text")
+    if super_div is None:
+        raise ValueError("Could not find number of results.")
+    p_tag = super_div.find("p")
+    if p_tag is None:
+        raise ValueError("Could not find p tag.")
+    if not isinstance(p_tag, Tag):
+        raise TypeError("p_tag must be a Tag.")
+    text = p_tag.get_text()
+    if not text.startswith("Hakuosumia "):
+        raise ValueError('Did not start with "Hakuosumia ".')
+    if not text.endswith(" kpl."):
+        raise ValueError('Did not end with " kpl.".')
+    num_text = text[len("Hakuosumia ") : -len(" kpl.")]
+    try:
+        return int(num_text)
+    except ValueError as e:
+        raise ValueError("Could not convert number of results to int.") from e
+
+
+def calc_upper_limit(num_pages: int) -> int:
+    """Calculate the upper limit for the offset."""
+    if num_pages % 20 == 0:
+        return num_pages
+    return num_pages + 20 - num_pages % 20
+
+
 def main() -> None:
     """Main function."""
     print("Running main!")
 
+    config = parse_config()
+
+    num_pages = get_num_pages(config.link)
+
+    upper_limit = calc_upper_limit(num_pages)
+
     result: list[DocumentEntry] = []
 
-    with Pool(40) as p:
-        res = p.map(pass_, range(0, 800, 20))
+    with Pool(upper_limit // 20) as p:
+        res = p.starmap(
+            pass_, map(lambda x: (x, config.link), range(0, upper_limit, 20))
+        )
         result.extend(i for y in res for i in y)
 
-    with open("output.csv", "wb") as f:
+    with open(config.output_file_name, "wb") as f:
         f.write(b"name,description,link\n")
         for entry in result:
             f.write(
