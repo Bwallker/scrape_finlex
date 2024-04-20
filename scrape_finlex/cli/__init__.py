d2fed96bba07795761f9054b6a7ca3514844f9d0
bwallker <benbwall@gmail.com>
2024-04-20 19:11:05 +0300

    Try adding language

--- scrape_finlex/cli/__init__.py
+++ scrape_finlex/cli/__init__.py
@@ -32,7 +32,7 @@ def get_page(offset: int, link: str) -> Response:
     driver.get(target)
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-        "Accept-Language": "en-US,en;q=0.9",
+        "Accept-Language": "fi-FI,en-US,en;q=0.9",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     }
 
