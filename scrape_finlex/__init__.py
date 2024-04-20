ab133c84ad97a2d1598464c9d00cd5e91d07a402
bwallker <benbwall@gmail.com>
2024-04-20 04:47:09 +0300

    Add dockerfile

--- scrape_finlex/__init__.py
+++ scrape_finlex/__init__.py
@@ -1,6 +1,4 @@
 """Init file for scrape-finlex."""
 
 from .cli import cli_main, do_scrape, Config
-from .server import server_main
-
-__version__ = "0.1.0"
+from .server import prod_main, server_main
--- scrape_finlex/server/__init__.py
+++ scrape_finlex/server/__init__.py
@@ -9,10 +9,15 @@ from .. import Config, do_scrape
 
 def server_main():
     """Run the Flask app."""
-    if __name__ == "__main__":
-        port = environ["PORT"]
-        if not port:
-            port = 5000
-        port = int(port)
-        app.run(port=port)
     return app
+
+
+def prod_main():
+    """Run the Flask app in production."""
+    port = environ["PORT"]
+    if not port:
+        port = 5000
+    port = int(port)
+    from waitress import serve
+
+    serve(app, host="0.0.0.0", port=port)
